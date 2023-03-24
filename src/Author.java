ce2ca6acc10754836579e276b5437b03d89d5dbe
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-03-24 10:13:22 -0500

    this commit fixed the error and warnings in author in a better way than the easy fix branch

--- src/Author.java
+++ src/Author.java
@@ -4,14 +4,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("unchecked")
 public class Author extends Person {
 
-    private List books;
+    private final List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -30,7 +29,8 @@ public class Author extends Person {
         books.add(book);
     }
 
+    @Override
     public String sortName() {
-        return String.format("%s %s", lastName, firstName);
+        return super.sortName();
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -14,4 +14,7 @@ public class Person {
         return String.format("%s %s", firstName, lastName);
     }
 
+    public String sortName(){
+        return String.format("%s %s", lastName, firstName);
+    }
 }
